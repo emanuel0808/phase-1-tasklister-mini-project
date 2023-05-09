5bacc781fa4849fbd60a7fb5ee108af89b640c79
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-05-09 14:01:17 -0700

    Add src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+addingEventListener()
 });
+
+function addingEventListener(){
+  document.getElementById("create-task-form")
+  .addEventListener("submit", handleFormSubmit)
+}
+
+function handleFormSubmit(e){
+  e.preventDefault()
+
+  const task = e.target[0].value
+
+  displayTask(task)
+}
+function displayTask(task) {
+  const taskUL = document.getElementById("tasks")
+  const taskLi = document.createElement("Li")
+  const deleteBtn = document.createElement("button")
+
+  deleteBtn.textContent = "x"
+  deleteBtn.addEventListener("click", deleteTask)
+
+
+  
+
+
+  taskLi.textContent = task + " "
+  taskLi.appendChild(deleteBtn)
+
+  taskUL.appendChild(taskLi)
+
+
+}
+function deleteTask(e){
+  console.log(e)
+  e.target.parentNode.remove()
+}
